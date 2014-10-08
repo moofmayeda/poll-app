f21bd82a151b2bead9133821bb9f7f6b24ee1868
moof mayeda <moofmayeda@yahoo.com>
2014-10-08 12:08:10 -0700

    show checkboxes that are pre-checked if completed

--- todos/urls.py
+++ todos/urls.py
@@ -3,4 +3,6 @@ from django.conf.urls import patterns, url
 from todos import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.IndexView.as_view(), name='index'))
+  url(r'^$', views.IndexView.as_view(), name='index'),
+  url(r'^update/$', views.update_completed, name='update_completed'),
+)
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.views import generic
 
 from todos.models import Category, Task
@@ -9,3 +11,15 @@ class IndexView(generic.ListView):
 
   def get_queryset(self):
     return Category.objects.all()
+
+def update_completed(request):
+  try:
+    selected_task = Task.objects.get(pk=request.POST['task'])
+  except (KeyError, Task.DoesNotExist):
+    return render(request, 'todos/index.html', {
+      'error_message': "You didn't select a choice.",
+      })
+  else:
+    selected_task.completed = True
+    selected_task.save()
+    return HttpResponseRedirect(reverse('todos:index'))
