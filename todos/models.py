73d72b426265b1cfed08726f7851f4fc724147b3
moof mayeda <moofmayeda@yahoo.com>
2014-10-08 09:19:20 -0700

    create total_tasks method for categories

--- todos/models.py
+++ todos/models.py
@@ -5,6 +5,8 @@ class Category(models.Model):
     category_text = models.CharField(max_length=200)
     def __unicode__(self):
       return self.category_text
+    def total_tasks(self):
+      return self.task_set.count()
 
 class Task(models.Model):
     category = models.ForeignKey(Category)
